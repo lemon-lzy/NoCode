86ad9005d5b15c1e8868427ee51db7b44bd5871e
罗子扬 <13662597+lzywantsleep@user.noreply.gitee.com>
2026-01-06 15:12:01 +0800

    项目初始化

---
+++ src/main/java/com/lzy/nocode/NoCodeApplication.java
@@ -0,0 +1,13 @@
+package com.lzy.nocode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lzy/nocode/NoCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzy.nocode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
