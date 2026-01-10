630320c78eb97e60b1dd4f4a716fdbc9b63d4043
罗子扬 <13662597+lzywantsleep@user.noreply.gitee.com>
2026-01-11 00:50:00 +0800

    用户模块

--- src/main/java/com/lzy/nocode/NoCodeApplication.java
+++ src/main/java/com/lzy/nocode/NoCodeApplication.java
@@ -1,11 +1,13 @@
 package com.lzy.nocode;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication
+@MapperScan("com.lzy.nocode.mapper")
 public class NoCodeApplication {
 
     public static void main(String[] args) {
