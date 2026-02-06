9465f3822ff091a70b9690c62daf1a0622e728c0
罗子扬 <13662597+lzywantsleep@user.noreply.gitee.com>
2026-02-06 11:41:43 +0800

    开发应用生成crud，修改SSE

---
+++ src/main/java/com/lzy/nocode/mapper/AppMapper.java
@@ -0,0 +1,13 @@
+package com.lzy.nocode.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.lzy.nocode.model.entity.App;
+
+/**
+ * 应用 映射层。
+ *
+ * @author lzy
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
---
+++ src/main/java/com/lzy/nocode/service/AppService.java
@@ -0,0 +1,50 @@
+package com.lzy.nocode.service;
+
+import com.lzy.nocode.model.dto.app.AppQueryRequest;
+import com.lzy.nocode.model.entity.User;
+import com.lzy.nocode.model.vo.AppVO;
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+import com.lzy.nocode.model.entity.App;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+/**
+ * 应用 服务层。
+ *
+ * @author lzy
+ */
+public interface AppService extends IService<App> {
+
+    /**
+     * App->appVo
+     * @param app
+     * @return
+     */
+    public AppVO getAppVO(App app);
+
+    /**
+     * 构建app查询相关的queryWrapper
+     * @param appQueryRequest
+     * @return
+     */
+    public QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+    /**
+     * List<App>->List<AppVO>(因为AppVO有携带用户信息所以需要连表查询)
+     * @param appList
+     * @return
+     */
+    public List<AppVO> getAppVOList(List<App> appList);
+
+    /**
+     * ai生成应用接口
+     * @param appId
+     * @param message 用户消息
+     * @param loginUser 登录用户
+     * @return
+     */
+    public Flux<String> chatToGenCode(Long appId, String message, User loginUser);
+
+}
