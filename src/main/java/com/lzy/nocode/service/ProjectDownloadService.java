c9ff45f654f7785d4abcbbd27b30bdbf1d1662cf
罗子扬 <13662597+lzywantsleep@user.noreply.gitee.com>
2026-02-14 01:14:51 +0800

    下载代码功能开发

---
+++ src/main/java/com/lzy/nocode/service/ProjectDownloadService.java
@@ -0,0 +1,8 @@
+package com.lzy.nocode.service;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface ProjectDownloadService {
+
+    public void downloadProjectAsZip(String projectPath, String downloadFileName, HttpServletResponse response);
+}
